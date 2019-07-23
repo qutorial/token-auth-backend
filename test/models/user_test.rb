917b08d6257fdc0724f78d65a1193d020112a728
Zaur Molotnikov <qutorial@gmail.com>
2019-07-23 17:59:42 +0200

    Devise token auth configurred

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "fixtures count" do
-     assert_equal 2, User.count
+    assert_equal 2, User.count
   end
   
+  test "email validations" do
+    assert_equal true, users(:one).valid?
+    assert_equal false, users(:two).valid?
+  end
+
 end
