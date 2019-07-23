ab0cffac39dbdcd11ebc101a766f9c26a803982a
Zaur Molotnikov <qutorial@gmail.com>
2019-07-23 18:14:34 +0200

    CSRF off

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
-        include DeviseTokenAuth::Concerns::SetUserByToken
-  protect_from_forgery with: :exception
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name nickname email])
+  end
 end
